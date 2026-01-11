a9aaa1cf164380f6a7d84871a279fb506708040d
Ryan Lieu <github-benzBrake@woai.ru>
2026-01-11 23:24:19 +0800

    feat: (JS) downloadPlus_Fx136.uc.js 使用内置图片代理移除内置base64编码图片

--- downloadPlus_Fx136.uc.js
+++ downloadPlus_Fx136.uc.js
@@ -1113,7 +1113,7 @@ userChromeJS.downloadPlus.enableSaveTo 下载对话框启用保存到
         } : null;
         var alertsService = Cc["@mozilla.org/alerts-service;1"].getService(Ci.nsIAlertsService);
         alertsService.showAlertNotification(
-            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiBmaWxsPSJjb250ZXh0LWZpbGwiIGZpbGwtb3BhY2l0eT0iY29udGV4dC1maWxsLW9wYWNpdHkiPjxwYXRoIGZpbGw9Im5vbmUiIGQ9Ik0wIDBoMjR2MjRIMHoiLz48cGF0aCBkPSJNMTIgMjJDNi40NzcgMjIgMiAxNy41MjMgMiAxMlM2LjQ3NyAyIDEyIDJzMTAgNC40NzcgMTAgMTAtNC40NzcgMTAtMTAgMTB6bTAtMmE4IDggMCAxIDAgMC0xNiA4IDggMCAwIDAgMCAxNnpNMTEgN2gydjJoLTJWN3ptMCA0aDJ2NmgtMnYtNnoiLz48L3N2Zz4=", aTitle || "DownloadPlus",
+            "chrome://global/skin/icons/info.svg", aTitle || "DownloadPlus",
             aMsg + "", !!callback, "", callback);
     }
 
