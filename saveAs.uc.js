f21ddee731c6bc4abac30cb5bb399ae010d8da27
Ryan Lieu <github-benzBrake@woai.ru>
2026-04-09 22:04:10 +0800

    fix: (JS) saveAs.uc.js 修复 Firefox 136 兼容性并增强文件名处理
    - 使用沙箱环境执行 internalSave 函数，修复 Firefox 136 兼容性问题
    - 添加文件名非法字符过滤功能，替换为下划线
    - 添加 mainWindow 和 internalSave 存在性检查，增强容错性
    - 更新兼容性版本至 Firefox 136
    - 更新版本号至 1.1.0
    - 代码现代化，将 let 改为 const

--- saveAs.uc.js
+++ saveAs.uc.js
@@ -6,9 +6,9 @@
 
 这个脚本最先是紫云飞开发的：https://files.cnblogs.com/ziyunfei/saveas.uc.js
 */
-// @version         1.0.0
+// @version         1.1.0
 // @license         MIT License
-// @compatibility   Firefox 72
+// @compatibility   Firefox 136
 // @charset         UTF-8
 // @include         chrome://mozapps/content/downloads/unknownContentType.xhtml
 // @include         chrome://mozapps/content/downloads/unknownContentType.xul
@@ -17,6 +17,8 @@
 (function () {
     if (!location.href.startsWith("chrome://mozapps/content/downloads/unknownContentType.x")) return;
     if (!Services.prefs.getBoolPref("userChromeJS.downloadPlus.enableSaveAs", true)) return;
+    const { utils: Cu } = Components;
+    const invalidChars = /[<>:"/\\|?*]/g;
     const $C = (doc, tag, props) => {
         const el = doc.createXULElement(tag);
         for (const [key, value] of Object.entries(props)) {
@@ -27,19 +29,67 @@
             }
         }
         return el;
-    }
+    };
+
+    const getMainWindowSandbox = mainWindow => {
+        if (mainWindow.DownloadPlus?.sb) {
+            return mainWindow.DownloadPlus.sb;
+        }
+        if (mainWindow.__saveAsSandbox) {
+            return mainWindow.__saveAsSandbox;
+        }
+
+        const sb = Cu.Sandbox(mainWindow, {
+            sandboxPrototype: mainWindow,
+            sameZoneAs: mainWindow,
+            freezeBuiltins: false
+        });
+        Cu.evalInSandbox(`
+            Function.prototype.toSource = window.Function.prototype.toSource;
+            Object.defineProperty(Function.prototype, "toSource", { enumerable: false });
+            Object.prototype.toSource = window.Object.prototype.toSource;
+            Object.defineProperty(Object.prototype, "toSource", { enumerable: false });
+            Array.prototype.toSource = window.Array.prototype.toSource;
+            Object.defineProperty(Array.prototype, "toSource", { enumerable: false });
+        `, sb);
+        mainWindow.addEventListener("unload", () => {
+            setTimeout(() => {
+                Cu.nukeSandbox(sb);
+            }, 0);
+        }, { once: true });
+        mainWindow.__saveAsSandbox = sb;
+        return sb;
+    };
+
     const { dialog } = window;
-    let saveAs = $C(document, 'button', {
+    const saveAs = $C(document, 'button', {
         id: 'save-as',
         label: "另存为",
         accesskey: 'E',
         oncommand: function () {
-            const mainwin = Services.wm.getMostRecentWindow("navigator:browser");
-            // 感谢 ycls006
-            mainwin.eval("(" + mainwin.internalSave.toString().replace("let ", "").replace("var fpParams", "fileInfo.fileExt=null;fileInfo.fileName=aDefaultFileName;var fpParams") + ")")(dialog.mLauncher.source.asciiSpec, null, null, (document.querySelector("#locationText") ? document.querySelector("#locationText").value : dialog.mLauncher.suggestedFileName), null, null, false, null, null, null, null, null, false, null, mainwin.PrivateBrowsingUtils.isBrowserPrivate(mainwin.gBrowser.selectedBrowser), Services.scriptSecurityManager.getSystemPrincipal());
+            const mainWindow = Services.wm.getMostRecentWindow("navigator:browser");
+            if (!mainWindow || typeof mainWindow.internalSave !== "function") {
+                return;
+            }
+
+            const fileName = document.querySelector("#locationText")?.value?.replace(invalidChars, "_")
+                || dialog.mLauncher.suggestedFileName;
+
+            // 感谢 ycls006 / alice0775
+            Cu.evalInSandbox(
+                "(" + mainWindow.internalSave.toString()
+                    .replace("let ", "")
+                    .replace("var fpParams", "fileInfo.fileExt=null;fileInfo.fileName=aDefaultFileName;var fpParams") + ")",
+                getMainWindowSandbox(mainWindow)
+            )(
+                dialog.mLauncher.source.asciiSpec, null, null, fileName,
+                null, null, false, null, null, null, null, null, false, null,
+                mainWindow.PrivateBrowsingUtils.isBrowserPrivate(mainWindow.gBrowser.selectedBrowser),
+                Services.scriptSecurityManager.getSystemPrincipal()
+            );
             close();
         }
     });
-    let ins = dialog.dialogElement('unknownContentType').getButton('cancel');
+    const ins = dialog.dialogElement('unknownContentType').getButton('cancel');
     ins.before(saveAs);
-})()
+})();
